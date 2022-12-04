3829d7487fccdb74045f92652a2eab5021ca951f
nianabila <116358955+nianabila@users.noreply.github.com>
2022-12-04 23:27:10 +0700

    Add files via upload

---
+++ Perangkat.java
@@ -0,0 +1,14 @@
+public class Perangkat {
+    protected String drive;
+    protected int ram;
+    protected float processor;
+
+    public Perangkat(String drive, int ram, float processor){
+        this.drive = drive;
+        this.ram = ram;
+        this.processor = processor;
+    }
+    public void informasi(){
+        System.out.println("Perangkat tidak diketahui memiliki tipe "+drive+" dengan ram sebesar "+ram+"GB dan processor secepat "+processor+" Ghz");
+    }
+} 
